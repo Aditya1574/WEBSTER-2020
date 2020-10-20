a2e5bf55dbb999d7549c262f0e5459c0ba44ffe9
Aditya1574 <62781581+Aditya1574@users.noreply.github.com>
2020-10-20 11:12:06 +0530

    Check

--- app.js
+++ app.js
@@ -27,36 +27,69 @@ app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/userDB", {useUnifiedTopology: true , useNewUrlParser: true});
 mongoose.set("useCreateIndex" , true);
 
-
-const userSchema  = new mongoose.Schema({
-username: String,
-password: String,
-googleId: String,
-secret: String
+const recruiterSchema = new mongoose.Schema({
+id: String,  
+Company: String,
+Recruiter_name: String,
+Recruiter_Pos: String,
+Recruiter_email: String,
+Company_email: String,
+Recruiter_Phno: String,
+Country: String,
+City: String,
+Experience: String,
+Level: String
 });
 
-userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);  // google added
+const projectSchema = new mongoose.Schema({
+  Name: String,
+  Desc: String,
+  link: String
+});
+
+const studentSchema  = new mongoose.Schema({
+email: String,
+password: String,
+googleId: String,
+first_name: String,
+last_name: String,
+highest_Quali: String,
+course: String,
+Specialization: String,
+College: String,
+course_type: String,
+Grad_Year: String,
+projects: [projectSchema],
+Skills: [String]
+});
+
+studentSchema.plugin(passportLocalMongoose); // for Student 
+recruiterSchema.plugin(passportLocalMongoose); //for Recruiter
+studentSchema.plugin(findOrCreate);  // google added
 
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", studentSchema);
+const project = mongoose.model("project" , projectSchema);
+const Recruiter = mongoose.model("recruiter", recruiterSchema);
 
-passport.use(User.createStrategy());
+passport.use(User.createStrategy()); // Student
+passport.use(Recruiter.createStrategy()); //Recruiter
 
-passport.serializeUser(function(user, done) {             // works for all the strategies // google added
+passport.serializeUser(function(user, done) {    //student // works for all the strategies // google added
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {          // works for all the strategies // google added
-  User.findById(id, function(err, user) {
-    done(err, user);
+passport.deserializeUser(function(id, done) {   //Student // works for all the strategies // google added                                              
+  User.findById(id, function(err, user) {   
+  done(err, user);
+  });
+  Recruiter.findById(id, function(err, user) {
+  done(err, user);
   });
 });
 
-
 let TempObj = {
-  name: "",
-  pass: null
+  name: ""
 }
 // google added
 passport.use(new GoogleStrategy({
@@ -88,9 +121,15 @@ app.get("/auth/google/secrets",
       // Done Finally                                               /* it is authenticated by the google serves */
     
       res.render("Student_login", {user: TempObj});
-    });
+});
 
-
+app.get("/Student_edit", function(req, res){
+if(req.isAuthenticated()){
+  res.render("Student_edit", {user: TempObj});
+}else{
+  res.redirect("/register");
+}
+});
 
 app.get("/login", function(req, res){
   res.render("login");
@@ -105,60 +144,83 @@ app.get("/logout" , function(req, res){
   res.redirect("/");
 });
 
+app.post("/Student_edit", function(req, res){
+  console.log(req.body);
+  let x  = req.body.skillwala;
+  let y = x.split(",");
+  console.log(y);
+});
 
 
-
-app.post("/register", function(req, res){
-const temp = {   // add the rest of the entries
-  name: req.body.username,
-  pass: req.body.password
-}
-
-User.register({username: req.body.username} ,req.body.password , function(err , user){
+app.post("/student_register", function(req, res){
+ 
+TempObj.name = req.body.username;
+User.register({username: req.body.username}, req.body.password , function(err , user){
   if(err){
-    console.log(err + " at register route" );
+    console.log(err + " at Student register route" );
     res.redirect("/register");
   }else{
     passport.authenticate("local")(req, res, function(){
-      if(req.body.IDENTITY === "STD"){
-        res.render("Student_edit" , {user: temp});
-        }
-        else{
-        res.render("Recuiter_edit", {user: temp});
-        }
+        res.redirect("/Student_edit");
     });
   }
 });
 
 });
 
-app.post("/login", function(req, res){
+app.post("/recruiter_register", function(req, res){
+TempObj.name = req.body.username;
+  Recruiter.register({username: req.body.username}, req.body.password, function(err, recruiter){
+     if(err){
+       console.log(err + " at Recruiter register Route");
+       res.redirect("/register");
+     }else{
+       passport.authenticate("local")(req, res, function(){
+        res.render("Recruiter_edit", {user: TempObj});
+       });
+     }
+   });
+});
+
+app.post("/student_login", function(req, res){
 const user = new User({                // add More entries here
-  username: req.body.username,
+  username: req.body.username0,
   password: req.body.password
 });
-const temp = {
-  name: req.body.username,
-  pass: req.body.password
-}
+
+TempObj.name = req.body.username;
+
 req.login(user, function(err){
   if(err){
     console.log(err);
     res.redirect("/login");
   }else{
     passport.authenticate("local")(req, res, function(){
-      if(req.body.IDENTITY === "STDLOGIN"){
-      res.render("Student_login" , {user: temp});
-      }
-      else{
-      res.render("Recruiter_login" , {user: temp});
-      }
+      res.render("Student_login" , {user: TempObj});
     });
   }
 });
 
 });
 
+app.post("/recruiter_login", function(req, res){
+  const recruiter = new Recruiter({
+   username: req.body.username,
+   password: req.body.password
+  });
+
+  TempObj.name = req.body.username;
+   
+  req.login(recruiter, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.render("Recruiter_login", {user: TempObj});
+      });
+    }
+  });
+});
 
 app.listen(3000, function(){
   console.log("The server is Running Properly at Port 3000");
