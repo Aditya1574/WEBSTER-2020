fa9b3a7905071a5a2f34820616844aeb3cf66d8e
Karan835452 <68146492+Karan835452@users.noreply.github.com>
2020-10-29 18:38:47 +0530

    added route for student_page.ejs

--- app.js
+++ app.js
@@ -240,7 +240,9 @@ app.get("/recruiter_loggedin", function(req, res){
   }
 });
 
-
+app.get("/student_page", function(req, res){
+  res.render("student_page");
+});
 app.get("/", function(req, res){
   res.render("home");
 });
@@ -419,7 +421,7 @@ req.login(user, function(err){
     res.redirect("/login");
   }else{
     passport.authenticate("local")(req, res, function(){
-      res.redirect("/student_loggedin");
+       res.redirect("/student_page");
     });
   }
 });
